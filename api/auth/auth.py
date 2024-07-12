93facc5be2423a699e2aa353d9b6e5da8e9e44f3
Shantralee-58 <lindiwekhumalo833@gmail.com>
2024-07-12 20:20:19 +0200

    drop down nav

---
+++ api/auth/auth.py
@@ -0,0 +1,29 @@
+from flask import Blueprint, render_template
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/register')
+def register():
+    return render_template('register.html')
+
+@auth.route('/login')
+def login():
+    return render_template('login.html')
+
+# main.py (Main Flask app file)
+from flask import Flask
+from auth import auth
+
+app = Flask(__name__)
+app.register_blueprint(auth, url_prefix='/auth')
+
+@app.route('/')
+def base():
+    return render_template('base.html')
+
+@app.route('/chat')
+def chat():
+    return "Chat with us page"
+
+if __name__ == '__main__':
+    app.run(debug=True)
