e80944e30b8a270301867a491b9066d27476ee75
CodeX1991 <oghenewonaholy@gmail.com>
2024-07-13 18:07:57 +0100

    Add login templates, handle api, databasestructure

--- api/auth/routes.py
+++ api/auth/routes.py
@@ -3,14 +3,16 @@
 
 # Imports
 from flask import render_template, flash, redirect, url_for
-from .forms import RegistrationForm
-from . import auth
-from .utils import hash_password
+from .forms import RegistrationForm, LoginForm
+from . import auth # Import from __int__.py
+from . import hash_password # Import from __int__.py
 from api import db
 from models import User
+from api.views import views
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
+    """Handle POST api/auth/register"""
     form = RegistrationForm()
     if form.validate_on_submit():
         # Process form data (e.g., create user, save to database)
@@ -25,10 +27,13 @@ def register():
         db.session.add(new_user)
         db.session.commit()
         flash('Your account has been created! You can now log in.', 'success')
-        return "successful"
-    return render_template('register.html', form=form)
+        return redirect(url_for('auth.login'))
+    return render_template('register.html', title='Register', form=form)
 
-
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    """Handle POST api/auth/login"""
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect (url_for('views.home'))
+    return render_template('login.html', title='Sign In', form=form)
