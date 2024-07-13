e012c3be40c6499a1abc63e6170a64084fb5d004
CodeX1991 <99116739+CodeX1991@users.noreply.github.com>
2024-07-14 00:13:44 +0100

    Delete api directory

--- api/views/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Create a Blueprint for routes"""
-
-from flask import Blueprint, render_template, url_for, redirect
-
-views = Blueprint('views', __name__)
-
-# Import routes to register with the Blueprint
-from api.views import routes
--- api/views/routes.py
+++
@@ -1,6 +0,0 @@
-from flask import render_template
-from api.views import views
-
-@views.route('/')
-def home():
-    return render_template("service_dir.html")
