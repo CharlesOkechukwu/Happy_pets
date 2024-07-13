b3f89eb00bf94eb625be6d6ffd322534e90fefa8
CharlesOkechukwu <realcharlieok@gmail.com>
2024-07-13 04:43:24 +0100

    style add_pet html page mobile and desktop version

--- api/views/routes.py
+++ api/views/routes.py
@@ -1,14 +1,25 @@
-from flask import render_template
+from flask import render_template, request
 from api.views import views
 
 @views.route('/')
 def home():
     return render_template("service_dir.html")
 
-@views.route('/pet', methods=['GET'], strict_slashes=False)
+@views.route('/pet', methods=['GET', ], strict_slashes=False)
 def get_pet():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        specie = request.form.get('specie')
+        breed = request.form.get('breed')
+        age = request.form.get('age')
+        color = request.form.get('color')
+        
     return render_template("add_pet.html")
 
 @views.route('/health', methods=['GET'], strict_slashes=False)
 def health():
     return render_template("health_tracker.html")
+
+@views.route('/base')
+def index():
+    return render_template('base.html')
