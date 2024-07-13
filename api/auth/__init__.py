55d2daaf2c723386d90ae9f85003d31692eae890
CharlesOkechukwu <realcharlieok@gmail.com>
2024-07-13 22:01:37 +0100

    commit changes

--- api/auth/__init__.py
+++ api/auth/__init__.py
@@ -5,5 +5,5 @@
 from flask import Blueprint, render_template, url_for, redirect
 
 auth = Blueprint('auth', __name__)
+from .utils import hash_password, verify_password 
 from api.auth import routes
-from .utils import hash_password
