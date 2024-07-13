d5771f7de4a1add00924376f9c20d007d0279486
CodeX1991 <99116739+CodeX1991@users.noreply.github.com>
2024-07-14 00:14:56 +0100

    Delete static directory

--- static/js/alert_btn.js
+++
@@ -1,9 +0,0 @@
-// Handling Alert button
-document.addEventListener('DOMContentLoaded', (event) => {
-    const closeButtons = document.querySelectorAll('.close-btn');
-    closeButtons.forEach(button => {
-      button.addEventListener('click', function() {
-        this.parentElement.style.display = 'none';
-      });
-    });
-  });
