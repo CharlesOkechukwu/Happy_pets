9342f3a4683038364adaeb70275b0e7428489922
CharlesOkechukwu <realcharlieok@gmail.com>
2024-07-14 18:25:54 +0100

    fixing css for health_tracker and add_pet frontend pages

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 """Run the flask app"""
 from api import create_app
 
-app = create_app('config.DevelopmentConfig')
+app = create_app('config.CharlesConfig')
 
 if __name__ == '__main__':
     app.run(debug=True)
