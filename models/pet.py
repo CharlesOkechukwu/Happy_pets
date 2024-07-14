069078aa968f437deb6fae7312bce03988dacfed
CharlesOkechukwu <realcharlieok@gmail.com>
2024-07-14 17:04:51 +0100

    merge changes

---
+++ models/pet.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""module for pet object model"""
+from api import db
+
+
+class Pet(db.Model):
+    """pet object model"""
+    __tablename__ = 'pet'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    specie = db.Column(db.String(200), nullable=False)
+    breed = db.Column(db.String(200), nullable=False)
+    birth_date = db.Column(db.Date, nullable=False)
+    color = db.Column(db.String(200), nullable=False)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    photo = db.Column(db.String(200), nullable=False)
