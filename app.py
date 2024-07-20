5beb3ed5ef5e833a4d5673a07b86f5df2e405d39
Charles C. Okechukwu <realcharlieok@gmail.com>
2024-07-20 07:01:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 """Run the flask app"""
 from api import create_app
 
-app = create_app('config.CharlesConfig')
+app = create_app('config.DevelopmentConfig')
 
 if __name__ == '__main__':
     app.run(debug=True)
